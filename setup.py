3d732c37e0a81dde6af4888421945ad62bc8de4b
shivakumarsanugula <shivakumaryadav1997@gmail.com>
2023-10-27 13:21:53 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="its a wine quality package",
+    author="shivakumar",
+    packages=find_packages(),
+    license="MIT"
+
+
+)
